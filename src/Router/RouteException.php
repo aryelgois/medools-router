7fca843ec6127fa6ec913ffe6fa1f5c4973ea85e
Aryel Mota Góis <aryel.gois@gmail.com>
2018-03-08 00:19:34 -0300

    Add RouteException

---
+++ src/Router/RouteException.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This Software is part of aryelgois/medools-router and is provided "as is".
+ *
+ * @see LICENSE
+ */
+
+namespace aryelgois\Medools\Router;
+
+/**
+ * Error detected when processing a route
+ *
+ * @author Aryel Mota Góis
+ * @license MIT
+ * @link https://www.github.com/aryelgois/medools-router
+ */
+class RouteException extends \Exception
+{
+    /**
+     * Response stored
+     *
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * Creates a new RouteException object
+     *
+     * @param Response  $response Response with error message
+     * @param Throwable $previous Previous exception for chaining
+     */
+    public function __construct(
+        Response $response,
+        Throwable $previous = null
+    ) {
+        $this->response = $response;
+
+        parent::__construct($response->body['message'], 0, $previous);
+    }
+
+    /**
+     * Returns stored response
+     *
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+}
