0d5d2376008887a4e716152a413271fca3704283
Aryel Mota Góis <aryel.gois@gmail.com>
2018-05-12 11:31:34 -0300

    Add from_globals()

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ### Added
 - Repository title
+- `from_globals()`
 
 ### Changed
 - Use `class` keyword in foreign classes
--- example/api/auth/index.php
+++ example/api/auth/index.php
@@ -4,18 +4,14 @@ require_once __DIR__ . '/../../bootstrap.php';
 
 use aryelgois\MedoolsRouter;
 
-// Request
-$headers = getallheaders();
-
-// Router
-$url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+$request = from_globals();
 
 $router_data = json_decode(file_get_contents(__DIR__ . '/../router.json'), true);
 
 $controller = new MedoolsRouter\Controller(
-    $url,
+    $request['url'],
     $router_data['resources'],
     $router_data['configurations']
 );
 
-$controller->authenticate($headers['Authorization'] ?? '');
+$controller->authenticate($request['headers']['Authorization'] ?? '');
